182e33a0b60f2cb3b0e306185bab0565e4a4e9fd
Thomas Sileo <thomas.sileo@gmail.com>
2016-08-18 16:27:47 +0200

    Tweak

--- gps.py
+++ gps.py
@@ -9,20 +9,24 @@ class GPS(EventType):
     PUBLISHER = True
 
     def __init__(self, config):
+        self.config = config
         super(GPS, self).__init__()
 
     def before_register(self):
         self.blueprint.add_url_rule('/', 'index', self.hello)
-        self.blueprint.add_url_rule('/append', 'append', self.append)
+        self.blueprint.add_url_rule('/append/'+self.config.get('append_path'), 'append', self.append)
     @auth.login_required
     def hello(self):
         return 'hello'
 
-    @auth.login_required
+    # @auth.login_required
     def append(self):
+        if 'tracker' in request.args:
+            print 'tracker', request.args.get('tracker')
+
         data = dict(
             lat=float(request.args.get('lat')),
-            long=float(request.args.get('long')),
+            lon=float(request.args.get('lon')),
         )
         self.db.input(data)
         self.publish('last', data)
